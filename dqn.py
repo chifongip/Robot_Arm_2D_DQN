8d31773068e03985d7a94194b599fdb0308d29b2
fongip00@gmail.com <fongip00@gmail.com>
2023-04-21 13:59:47 +0800

    "code revision - 0421"

--- dqn.py
+++ dqn.py
@@ -40,7 +40,7 @@ class DQN(object):
     def act(self, s, epsilon=0.5):
         decay_constant = 0.0002
 
-        # At the beginning, generate more states        
+        # at the beginning, generate more states        
         if self.step == 0:
             if np.random.uniform() < 0.8:
                 return np.random.choice([0, 1, 2, 3]) 
@@ -52,34 +52,35 @@ class DQN(object):
                 return np.random.choice([0, 1, 2, 3]) 
             return np.argmax(self.model.predict(np.array([s]))[0])
         
-        # Action is chosen based on decaying epsilon-greedy behaviour
+        # action is chosen based on decaying epsilon-greedy behaviour
         else:
             if np.random.uniform() < epsilon - self.step * decay_constant:
                 return np.random.choice([0, 1, 2, 3]) 
             return np.argmax(self.model.predict(np.array([s]))[0])
 
 
-    # Save the trained model
-    def save_model(self, file_path='Models/test_arm_model-v7-dqn.h5'):
+    # save the trained model
+    def save_model(self, file_path='models/test_arm_model-v7-dqn.h5'):
         print('Model saved')
         self.model.save(file_path)
 
 
-    # Store experiences in a replay memory, randomly sampled during nn training
+    # store experiences in a replay memory, randomly sampled during nn training
     def remember(self, s, a, next_s, reward):
         """Addtional rewards when robotic arm approaches target/goal for faster convergence"""
         # observation space : [joint_1_theta, joint_2_theta, target_tip_dis_x, target_tip_dis_y, goal_tip_dis_x, goal_tip_dis_y, PnP_action]
         
-        #if arm approaches target and PnP_action == 0
+        # if arm approaches target and PnP_action == 0
         if abs(next_s[2]) <= 40 and abs(next_s[3]) <= 40 and next_s[6] == 0:
             reward += 0.5
-        #if arm approaches goal and PnP_action == 1
+        # if arm approaches goal and PnP_action == 1
         elif abs(next_s[4]) <= 100 and abs(next_s[5]) <= 100 and next_s[6] == 1:
             reward += 0.5
-        self.replay_queue.append((s, a, next_s, reward))              #store replay memory
+
+        self.replay_queue.append((s, a, next_s, reward))              # store replay memory
 
 
-    # batch_size, lr : learning rate, dis_factor : discount factor
+    # batch_size, lr: learning rate, dis_factor: discount factor
     def train(self, batch_size=64, lr=0.9, dis_factor=0.9):
         if len(self.replay_queue) < self.replay_size:
             return
@@ -92,13 +93,13 @@ class DQN(object):
         s_batch = np.array([replay[0] for replay in replay_batch])
         next_s_batch = np.array([replay[2] for replay in replay_batch])
 
-        Q = self.model.predict(s_batch)                           #Q is calculated using main model
-        Q_next = self.target_model.predict(next_s_batch)          #Q_next is calculated using target model (prediction of next step)
+        Q = self.model.predict(s_batch)                           # Q is calculated using main model
+        Q_next = self.target_model.predict(next_s_batch)          # Q_next is calculated using target model (prediction of next step)
 
         # Q-learning
         for i, replay in enumerate(replay_batch):
             _, a, _, reward = replay
-            Q[i][a] = (1 - lr) * Q[i][a] + lr * (reward + dis_factor * np.amax(Q_next[i]))              #Update Q table using update rule
+            Q[i][a] = (1 - lr) * Q[i][a] + lr * (reward + dis_factor * np.amax(Q_next[i]))      # Update Q table using update rule
  
         # pass into the network for training
         self.model.fit(s_batch, Q, verbose=0)
@@ -120,20 +121,21 @@ for i in range(episodes):
         #     env.render()
         # else:
         #     env.close() 
-        a = agent.act(s)                           # choose an action based on Epsilon Greedy behaviour
-        next_s, reward, done, _ = env.step(a)      # Take action to get the next step and its reward, info
+        a = agent.act(s)                           # choose an action based on epsilon-greedy behaviour
+        next_s, reward, done, _ = env.step(a)      # take action to get the next step and its reward, info
         agent.remember(s, a, next_s, reward)       # store experience in replay memory
-        agent.train()                              # Train : Model weight adjusting, Q learning
+        agent.train()                              # train : model weight adjusting, Q learning
         score += reward                            # acumulate score
         s = next_s                                 # update state
-        if done or step_count == 150:              # episode termination
+        if done or step_count == 1000:             # episode termination
             score_list.append(score)               # store score in score_list
             print('episode:', i, 'score:', score, 'max:', max(score_list), 'mean',current_mean)
             break
         step_count += 1
-    # Stop and save the model when the average score of the last 30 times is greater than termination score
-    current_mean = np.mean(score_list[-50:])       # calculate the mean of the last 35 scores
-    if current_mean >= termination_score:          # Training Termination condition
+
+    # stop and save the model when the average score of the last 50 times is greater than termination score
+    current_mean = np.mean(score_list[-50:])       # calculate the mean of the last 50 scores
+    if current_mean >= termination_score:          # training termination condition
         print("Training is completed")             
         agent.save_model()                         # save the model 
         break
@@ -144,10 +146,10 @@ env.close()
 arr_rounded = np.round(score_list, decimals=2)
 print(arr_rounded)
 
-with open("CNN1.txt", "w") as file:
+with open("figs/test_model_v7.txt", "w") as file:
     file.write(str(arr_rounded))
 
 plt.plot(score_list, color='green')
 plt.xlabel('Episodes')
 plt.ylabel('Scores')
-plt.savefig('CNN1.jpg')
+plt.savefig('figs/test_model_v7.jpg')
